50986f136cf91c0e7634b2c8c39a885959a4f6cc
aso1201771 <1201771@st.asojuku.ac.jp>
2014-06-06 11:00:25 +0900

    DB

--- src/jp/ac/st/asijuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asijuku/original2014002/MainActivity.java
@@ -1,22 +1,41 @@
 package jp.ac.st.asijuku.original2014002;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		Button mainteBtn = (Button)findViewById(R.id.mainteBtn);
+		mainteBtn.setOnClickListener(this);
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()){
+		case R.id.mainteBtn:
+			Intent intent = new Intent(MainActivity.this, MaintenanceActivity.class);
+			startActivity(intent);
+		}
+
 	}
 
 }
